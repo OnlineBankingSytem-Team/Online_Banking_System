c685570ca65510077b35292dd7d4b7bd9d911143
Nagihan Üstün <1170606055@nku.edu.tr>
2021-05-03 18:04:22 +0300

    New Attribute
    Branch Account and User Repositories are created. Admin and Bank Teller Contollers are updated.

--- onlinebankingsystem/src/main/java/com/onlinebankingsystem/onlinebankingsystem/controllers/AdminController.java
+++ onlinebankingsystem/src/main/java/com/onlinebankingsystem/onlinebankingsystem/controllers/AdminController.java
@@ -1,13 +1,27 @@
 package com.onlinebankingsystem.onlinebankingsystem.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.onlinebankingsystem.onlinebankingsystem.models.Account;
 import com.onlinebankingsystem.onlinebankingsystem.models.Admin;
+import com.onlinebankingsystem.onlinebankingsystem.models.BankTeller;
+import com.onlinebankingsystem.onlinebankingsystem.models.Branch;
+import com.onlinebankingsystem.onlinebankingsystem.models.User;
+
 import java.util.List;
+
+import com.onlinebankingsystem.onlinebankingsystem.repositories.IAccountRepository;
 import com.onlinebankingsystem.onlinebankingsystem.repositories.IAdminRepository;
+import com.onlinebankingsystem.onlinebankingsystem.repositories.IBankTellerRepository;
+import com.onlinebankingsystem.onlinebankingsystem.repositories.IBranchRepository;
+import com.onlinebankingsystem.onlinebankingsystem.repositories.IUserRepository;
 
 
 @RestController
@@ -16,19 +30,105 @@ public class AdminController {
 	  	@Autowired
 	    private IAdminRepository adminRepository;
 	  	
-	  	 public AdminController(IAdminRepository AdminRepository)
+	  	@Autowired
+	  	private IUserRepository userRepository;
+	  	
+	  	@Autowired
+	  	private IBankTellerRepository bankTellerRepository;
+	  	
+	  	@Autowired 
+	  	private IBranchRepository branchRepository;
+	  	
+	  	@Autowired
+	  	private IAccountRepository accountRepository;
+	  	
+	  	 public AdminController(IAdminRepository AdminRepository, 
+	  			 IUserRepository UserRepository, 
+	  			 IBankTellerRepository BankTellerRepository,
+	  			 IBranchRepository BranchRepository,
+	  			 IAccountRepository AccountRepository)
 	     {
 	            adminRepository = AdminRepository;
+	            userRepository = UserRepository;
+	            bankTellerRepository = BankTellerRepository;
+	            branchRepository = BranchRepository;
+	            accountRepository = AccountRepository;
 	     }
 
+	  	//Get All Admins
 	    @GetMapping("/admins")
-	    public List<Admin> All(){
+	    public List<Admin> getAllAdmins(){
 	        var admins = adminRepository.findAll();
 	        return admins;
 	    }
-
+	    
+	    //Get Admin By Id
 	    @GetMapping("/admins/{id}")
-	    public Admin get(@PathVariable Integer id ){
-	        return adminRepository.getOne(id);
+	    public Admin getAdminById(@PathVariable Integer id) {	      
+	      return adminRepository.getOne(id); 
 	    }
+	    
+	    //Get All Users
+	    @GetMapping("/admins/allUser")
+	    public List<User> getAllUser(){
+	    	return userRepository.findAll();
+	    } 
+	    
+	    //Get User By Id
+	    @GetMapping("/admins/allUser/{id}")
+	    public User getUserById(@PathVariable Integer Id) {
+	      return userRepository.getOne(Id); 
+	    }
+	    
+	    // Get All Bank Tellers
+	    @GetMapping("/admins/allBankTeller")
+	    public List<BankTeller> getAllBankTeller(){
+	    	return bankTellerRepository.findAll();
+	    }
+	    
+	    // Get Bank Teller By Id 
+	    @GetMapping("/admins/allBankTeller/{id}")
+	    public BankTeller getBankTellerById(@PathVariable Integer id) {
+	      return bankTellerRepository.getOne(id); 
+	    }
+	    
+	    // Get All Branchs
+	    @GetMapping("/admins/allBranch")
+	    public List<Branch> getAllBranch(){
+	    	return branchRepository.findAll();
+	    }
+	    
+	    //Create User
+	    @PostMapping("/admins/users")
+	    public User createUser(@RequestBody User newUser) {
+	      return userRepository.save(newUser);
+	    }
+	    
+	    //Delete User
+	    @DeleteMapping("/admins/users/{id}")
+	    void deleteEmployee(@PathVariable Integer id) {
+	      userRepository.deleteById(id);
+	    }
+	    
+	    //Update User
+	    @PutMapping("/admins/users/{id}")
+	    public User updateUser(@RequestBody User newUser, @PathVariable Integer id) {  
+	    	 return userRepository.findById(id)
+	    		      .map(user -> {
+	    		        user.setUserName(newUser.getUserName());
+	    		        user.setUserSurname(newUser.getUserSurname());
+	    		        user.setUserIdentityNo(newUser.getUserIdentityNo());
+	    		        return userRepository.save(user);
+	    		      })
+	    		      .orElseGet(() -> {
+	    		    	  newUser.setUserId(id);
+	    		    	  return userRepository.save(newUser);
+	    		      }); 		   
+	    }
+	    
+	    //Get All Account
+	    @GetMapping("/admins/allAccount")
+	    public List<Account> getAllAccount(){
+	    	return accountRepository.findAll();
+	    } 
 }
--- onlinebankingsystem/src/main/java/com/onlinebankingsystem/onlinebankingsystem/controllers/BankTellerController.java
+++ onlinebankingsystem/src/main/java/com/onlinebankingsystem/onlinebankingsystem/controllers/BankTellerController.java
@@ -5,8 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.onlinebankingsystem.onlinebankingsystem.models.Account;
 import com.onlinebankingsystem.onlinebankingsystem.models.BankTeller;
+import com.onlinebankingsystem.onlinebankingsystem.models.Branch;
+import com.onlinebankingsystem.onlinebankingsystem.models.User;
+
+import com.onlinebankingsystem.onlinebankingsystem.repositories.IAccountRepository;
 import com.onlinebankingsystem.onlinebankingsystem.repositories.IBankTellerRepository;
+import com.onlinebankingsystem.onlinebankingsystem.repositories.IBranchRepository;
+import com.onlinebankingsystem.onlinebankingsystem.repositories.IUserRepository;
 
 import java.util.List;
 
@@ -15,19 +22,53 @@ public class BankTellerController {
   	@Autowired
     private IBankTellerRepository bankTellerRepository;
   	
-  	 public BankTellerController(IBankTellerRepository BankTellerRepository)
+  	@Autowired
+  	private IUserRepository userRepository;
+  	
+  	@Autowired 
+  	private IBranchRepository branchRepository;
+  	
+  	@Autowired
+  	private IAccountRepository accountRepository;
+  	
+  	 public BankTellerController(IBankTellerRepository BankTellerRepository,
+  			 IUserRepository UserRepository, 
+  			 IBranchRepository BranchRepository,
+  			 IAccountRepository AccountRepository)
      {
             bankTellerRepository = BankTellerRepository;
+            userRepository = UserRepository;
+            branchRepository = BranchRepository;
+            accountRepository = AccountRepository;
      }
 
+  	//Get All Bank Tellers
     @GetMapping("/banktellers")
-    public List<BankTeller> All(){
+    public List<BankTeller> getAllBankTeller(){
         return bankTellerRepository.findAll();
         
     }
-
+    //Get Bank Teller By Id
     @GetMapping("/banktellers/{id}")
-    public BankTeller get(@PathVariable Integer id ){
+    public BankTeller getBankTellerById(@PathVariable Integer id ){
         return bankTellerRepository.getOne(id);
     }
+    
+    //Get All Users
+    @GetMapping("/banktellers/allUser")
+    public List<User> getAllUser(){
+    	return userRepository.findAll();
+    } 
+    
+    // Get All Branchs
+    @GetMapping("/banktellers/allBranch")
+    public List<Branch> getAllBranch(){
+    	return branchRepository.findAll();
+    }
+    
+    //Get All Account
+    @GetMapping("/banktellers/allAccount")
+    public List<Account> getAllAccount(){
+    	return accountRepository.findAll();
+    } 
 }
